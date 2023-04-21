26da1e34f9b27ee3ba04484df8903f6ff4794fe6
redpangilinan <redpangilinan715@gmail.com>
2023-04-22 02:10:52 +0800

    Added no results message when table is empty

--- src/crud/categories/read.php
+++ src/crud/categories/read.php
@@ -12,19 +12,24 @@ $stmt = $conn->prepare($sql);
 $stmt->execute();
 $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
-foreach ($result as $row) { ?>
-    <tr>
-        <td><?php echo $row["category_id"] ?></td>
-        <td><?php echo $row["category"] ?></td>
-        <td><?php echo $row["weight"] . "%" ?></td>
-        <td>
-            <div class="btn-group" role="group" aria-label="modify">
-                <button data-id="<?php echo $row["category_id"] ?>" class="edit-data btn btn-outline-dark" data-bs-toggle="modal" data-bs-target="#editModal"><i class="fas fa-edit"></i></button>
-                <button data-id="<?php echo $row["category_id"] ?>" class="delete-data btn btn-outline-dark"><i class="fas fa-trash"></i></button>
-            </div>
-        </td>
-    </tr>
+if (!empty($result)) {
+    foreach ($result as $row) { ?>
+        <tr>
+            <td><?php echo $row["category_id"] ?></td>
+            <td><?php echo $row["category"] ?></td>
+            <td><?php echo $row["weight"] . "%" ?></td>
+            <td>
+                <div class="btn-group" role="group" aria-label="modify">
+                    <button data-id="<?php echo $row["category_id"] ?>" class="edit-data btn btn-outline-dark" data-bs-toggle="modal" data-bs-target="#editModal"><i class="fas fa-edit"></i></button>
+                    <button data-id="<?php echo $row["category_id"] ?>" class="delete-data btn btn-outline-dark"><i class="fas fa-trash"></i></button>
+                </div>
+            </td>
+        </tr>
 <?php
+    }
+} else {
+    echo "<tr><td colspan='4'>No results found.</td></tr>";
 }
+
 $stmt = null;
 $conn = null;
