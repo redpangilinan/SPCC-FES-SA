9e70e16871f639c560a56cc5ce02e5eaa89174ac
redpangilinan <redpangilinan715@gmail.com>
2023-04-22 16:26:15 +0800

    Fixed edit form typo

--- templates/pages/questions.php
+++ templates/pages/questions.php
@@ -70,7 +70,7 @@ session_start();
         <div class="modal-dialog">
             <div class="modal-content">
                 <div class="modal-header">
-                    <h1 class="modal-title fs-5" id="editModalLabel">Edit Category</h1>
+                    <h1 class="modal-title fs-5" id="editModalLabel">Edit Questions</h1>
                     <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                 </div>
                 <!-- Edit Form -->
