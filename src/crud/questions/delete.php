f87ec8fd1dc18e46f374879830bfe42312950fd6
redpangilinan <redpangilinan715@gmail.com>
2023-04-22 18:17:04 +0800

    Fixed question_id on delete query

--- src/crud/questions/delete.php
+++ src/crud/questions/delete.php
@@ -2,7 +2,7 @@
 require "../../../config/connection.php";
 
 $primary_id = $_POST['delete_id'];
-$stmt = $conn->prepare("DELETE FROM tb_categories WHERE category_id = ?");
+$stmt = $conn->prepare("DELETE FROM tb_questions WHERE question_id = ?");
 if ($stmt->execute([$primary_id])) {
     echo "success";
 } else {
