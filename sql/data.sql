9fb0cd23bd540c0fc6bfbcd2db702765a037d7b3
redpangilinan <redpangilinan715@gmail.com>
2023-04-23 04:46:11 +0800

    Added 2 new tables and a sample admin data

---
+++ sql/data.sql
@@ -0,0 +1,15 @@
+INSERT INTO
+    tb_users (
+        user_type,
+        email,
+        password,
+        firstname,
+        lastname
+    )
+VALUES (
+        'admin',
+        'admin@gmail.com',
+        '$2y$10$2a6suV9Wa9TtmZJW6RAKjOVlFKybXxz.eARFqMCASyZket9HYP6Hi',
+        'Admin',
+        'Main'
+    );
--- sql/db.sql
+++ sql/db.sql
@@ -65,6 +65,28 @@ CREATE TABLE
     tb_verification (
         verification_id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
         email VARCHAR(255) NOT NULL,
-        otp INT(6) NOT NULL,
+        verification_code VARCHAR(10) NOT NULL,
         timestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+    );
+
+ALTER TABLE `tb_verification` ADD UNIQUE(`email`);
+
+CREATE TABLE
+    tb_access_codes (
+        access_id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+        access_name VARCHAR(255) NOT NULL,
+        access_code VARCHAR(255) NOT NULL,
+        created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+        updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+    );
+
+CREATE TABLE
+    tb_api_keys (
+        api_id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+        user_id INT UNSIGNED NOT NULL,
+        api_name VARCHAR(255) NOT NULL,
+        key_value VARCHAR(255) NOT NULL,
+        created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+        updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+        FOREIGN KEY (user_id) REFERENCES tb_users(user_id)
     );
