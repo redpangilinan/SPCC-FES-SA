e2c899b5c4544254d06849c33487290293ac3fe1
redpangilinan <redpangilinan715@gmail.com>
2023-04-23 15:41:29 +0800

    Added placeholder data for API keys and activation codes

--- sql/data.sql
+++ sql/data.sql
@@ -13,3 +13,21 @@ VALUES (
         'ADMIN',
         'MAIN'
     );
+
+INSERT INTO
+    tb_activation_codes (
+        activation_code,
+        activation_type
+    )
+VALUES ('ABC123', 'faculty');
+
+INSERT INTO
+    tb_activation_codes (
+        activation_code,
+        activation_type
+    )
+VALUES ('DEF456', 'admin');
+
+INSERT INTO
+    tb_api_keys (user_id, api_name, key_value)
+VALUES (1, 'OpenAI API Key', '');
