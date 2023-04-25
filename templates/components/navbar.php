8a21699d700a6e637f3c2be03d540d7e2a33e177
JDterrenal <johnderickramosterrenal@gmail.com>
2023-04-26 00:15:33 +0800

    navbar update

---
+++ templates/components/navbar.php
@@ -0,0 +1,51 @@
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css">
+    <link rel="stylesheet" href="https://cdn.datatables.net/1.13.4/css/jquery.dataTables.min.css">
+    <link rel="stylesheet" href="https://cdn.datatables.net/buttons/2.3.6/css/buttons.dataTables.min.css">
+    <link rel="stylesheet" href="../../public/css/style.css">
+    <title>Categories</title>
+</head>
+
+<div class="d-flex justify-content-end topbar">
+    <div class="topbar-logout-sec">
+        <a class="topbar-profile"><img src=""></a>
+        <a class="topbar-username">asd</a>
+    </div>
+</div>
+
+<div class="sidebar">
+	<nav>
+		<div class="sidebar-header">
+			<div class="sidebar-logo">
+				<a href="#" class="username"><img src="#" alt="" class="profile-side"></a>
+			</div>
+			<div class="sidebar-name">
+				<span class="sidebar-name1">Systems Plus</span>
+				<span class="sidebar-name1">Computer College</span>
+			</div>
+		</div>
+		<i class='fas fa-bars toggle'></i>
+	</nav>
+	<div class="menu-bar">
+		<div class="menu">
+			<nav>
+				<p><a href="#" class="username"><img src="#" alt="" class="profile-side"><span>Name</span></a></p>
+				<ul>
+				<li><a href="#"><i class="fas fa-desktop i"></i><span>Dashboard</span></a></li>
+                <li><a href="#"><i class="fas fa-user i"></i><span>Students</span></a></li>
+                <li><a href="#"><i class="fa fa-list-alt i"></i><span>Categories</span></a></li>
+                <li><a href="#"><i class="fas fa-users i"></i><span>Users</span></a></li>
+                <li><a href="#"><i class="fas fa-calendar-check i"></i><span>Evaluations</span></a></li>
+                <li><a href="#"><i class="fas fa-chalkboard-teacher i"></i><span>Faculty</span></a></li>
+                <li><a href="#"><i class="fas fa-question-circle i"></i><span>Question</span></a></li>
+                <li><a href="#"><i class="fa fa-gear i"></i><span>Settings</span></a></li>
+				<li><a href="?logout=true"><i class="fas fa-sign-out-alt i"></i><span>Sign Out</span></a></li>
+				</ul>
+			</nav>
+		</div>
+	</div>
+</div>
