0557da46ff6370342c45488821eb5e9023c5ce3c
redpangilinan <redpangilinan715@gmail.com>
2023-05-06 03:02:43 +0800

    Account activation is now based on actual available SPCC students

--- src/crud/users/create.php
+++ src/crud/users/create.php
@@ -1,56 +1,66 @@
 <?php
 require "../../../config/connection.php";
+if (session_status() === PHP_SESSION_NONE) {
+    session_start();
+}
 
 // Store input values into variables
-$user_type = $_POST['user_type'];
-$email = $_POST['email'];
+$user_type = "student";
+$email = $_SESSION['verify_email'];
 $password = $_POST['password'];
 $confirm_password = $_POST['confirm_password'];
-$activation_code = $_POST['activation_code'];
-$verification_code = $_POST['verification_code'];
+$verification_input = $_POST['verification_code'];
+$verification_code = $_SESSION['verification_code'];
 
-// Extract first and last name from email
-list($username, $domain) = explode('@', $email);
-$name_parts = explode('.', $username);
-$lastname = strtoupper(array_shift($name_parts));
-$firstname = implode(' ', array_map('strtoupper', $name_parts));
+$stmt = $conn->prepare('SELECT student_id, firstname, lastname FROM tb_students WHERE email = :email');
+$stmt->bindParam(':email', $email);
+$stmt->execute();
+$row = $stmt->fetch(PDO::FETCH_ASSOC);
 
-// Validate user privilege
-$valid = false;
-if ($user_type == "student") {
-    $valid = true;
-} else {
-    $stmt = $conn->prepare('SELECT activation_code FROM tb_activation_codes WHERE activation_type = ?');
-    $stmt->execute([$user_type]);
-    $confirm_activation_code = $stmt->fetchColumn();
-    if ($activation_code == $confirm_activation_code) {
+if ($row) {
+    $student_id = $row['student_id'];
+    $firstname = $row['firstname'];
+    $lastname = $row['lastname'];
+
+    // Validate verification code
+    $valid = false;
+    if ($verification_input == $verification_code) {
         $valid = true;
     } else {
-        echo "invalid_activation_code";
+        echo "invalid_verification_code";
+        exit();
     }
-}
 
-// Sanitize the user input before inserting record
-if ($valid) {
-    if ($password == $confirm_password) {
-        include "../../helpers/password_validation.php";
-        if (validatePassword($password)) {
-            $hashed_password = password_hash($password, PASSWORD_DEFAULT);
-            $stmt = $conn->prepare("INSERT INTO tb_users (user_type, email, password, firstname, lastname) VALUES (?, ?, ?, ?, ?)");
-            if ($stmt->execute([$user_type, $email, $hashed_password, $firstname, $lastname])) {
-                $stmt = $conn->prepare("DELETE FROM tb_verification WHERE email = ? AND verification_code = ?");
-                $stmt->execute([$email, $verification_code]);
-                echo "success";
+    // Sanitize the user input before inserting record
+    if ($valid) {
+        if ($password == $confirm_password) {
+            include "../../helpers/password_validation.php";
+            if (validatePassword($password)) {
+                $hashed_password = password_hash($password, PASSWORD_DEFAULT);
+                $stmt = $conn->prepare("INSERT INTO tb_users (student_id, user_type, email, password, firstname, lastname) VALUES (?, ?, ?, ?, ?, ?)");
+                if ($stmt->execute([$student_id, $user_type, $email, $hashed_password, $firstname, $lastname])) {
+                    $stmt = $conn->prepare("DELETE FROM tb_verification WHERE email = ? AND verification_code = ?");
+                    $stmt->execute([$email, $verification_code]);
+                    unset($_SESSION['verify_email']);
+                    unset($_SESSION['verification_code']);
+                    echo "success";
+                    exit();
+                } else {
+                    echo "Error: " . $stmt->errorInfo()[2];
+                    exit();
+                }
             } else {
-                echo "Error: " . $stmt->errorInfo()[2];
+                echo "weak_password";
+                exit();
             }
         } else {
-            echo "weak_password";
+            echo "password_mismatch";
+            exit();
         }
-    } else {
-        echo "password_mismatch";
     }
-}
 
-$stmt = null;
-$conn = null;
+    $stmt = null;
+    $conn = null;
+} else {
+    echo "invalid_email";
+}
